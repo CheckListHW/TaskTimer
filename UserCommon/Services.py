7f5c270e9730cc75b357f4c12f7f50b71ea92517
Иван Косачев <kosach_1@live.com>
2021-02-24 12:05:53 +0700

    refactor

---
+++ UserCommon/Services.py
@@ -0,0 +1,46 @@
+from typing import Optional
+from django.contrib.auth.models import User
+from UserAdmin.models import models
+from .models import ProjectHistory, ProjectActive
+from django.utils import timezone
+from UserAdmin import models as admin_models
+
+
+def start_project_active(project_id: int) -> Optional[bool]:
+    try:
+        project_history = ProjectHistory.objects.get(id=project_id)
+        if project_history.Start is None:
+            project_history.Start = timezone.now()
+            project_history.Activity = True
+            project_history.save()
+        return True
+    except Exception:
+        return False
+
+
+def stop_project_active(project_id: int) -> Optional[bool]:
+    try:
+        project_history = ProjectHistory.objects.get(id=project_id)
+        if project_history.Activity:
+            project_history.End = timezone.now()
+            project_history.Activity = False
+            project_history.save()
+        return True
+    except Exception:
+        return False
+
+
+def add_project_active(project_id: int, user: User) -> Optional[int]:
+    try:
+        main_project = admin_models.Project.objects.get(id=project_id)
+        project_active, created = ProjectActive.objects.get_or_create(Owner=user,
+                                                                      Project=main_project, Name=main_project.Name)
+        if not created:
+            project_active.save()
+        project_history = ProjectHistory(ProjectActive=project_active,
+                                         Name=main_project.Name,
+                                         Play=False)
+        project_history.save()
+        return project_history.id
+    except Exception:
+        return -1
--- UserCommon/views.py
+++ UserCommon/views.py
@@ -1,76 +1,28 @@
-from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from rest_framework.utils import json
-from django.utils import timezone
 
-
-from .models import *
+from .Services import *
 from UserAdmin import models as admin_models
-from UserAdmin import views as admin_views
-
-
-def checkauth(request, url, data=None):
-    if request.user.is_authenticated:
-        if request.user.groups.get().name == 'common':
-            return render(request, url, context=data)
-        else:
-            return render(request, 'Errors/ErrCommon.html')
-    return redirect(admin_views.start)
-
-
-def LogoutView(request):
-    logout(request)
-    return redirect(admin_views.start)
 
 
 def main(request):
     return render(request, 'Common/Main.html')
 
 
-def start_project(request):
-    try:
-        body_unicode = request.body.decode('utf-8')
-        body = json.loads(body_unicode)
-        project_history = ProjectHistory.objects.get(id=body.get('project_id'))
-        if project_history.Start is None:
-            project_history.Start = timezone.now()
-            project_history.Activity = True
-            project_history.save()
-        return HttpResponse(True)
-    except Exception:
-        return HttpResponse(False)
-
-
-def stop_project(request):
-    try:
-        body_unicode = request.body.decode('utf-8')
-        body = json.loads(body_unicode)
-        project_history = ProjectHistory.objects.get(id=body.get('project_id'))
-        if project_history.Activity:
-            project_history.End = timezone.now()
-            project_history.Activity = False
-            project_history.save()
-        return HttpResponse(True)
-    except Exception:
-        return HttpResponse(False)
-
-
-
-def add_project_views(request):
-#try:
-    user = request.user
+def start_project_active_views(request):
     body_unicode = request.body.decode('utf-8')
     body = json.loads(body_unicode)
-    main_project = admin_models.Project.objects.get(id=body.get('project_id'))
-    project_active, created = ProjectActive.objects.get_or_create(Owner=user,
-                                                                  Project=main_project, Name=main_project.Name)
-    if not created:
-        project_active.save()
-    project_history = ProjectHistory(ProjectActive=project_active,
-                                     Name=main_project.Name,
-                                     Play=False)
-    project_history.save()
-    return HttpResponse(project_history.id)
-#except Exception:
-    return HttpResponse(-1)
+    return HttpResponse(start_project_active(body.get('project_id')))
+
+
+def stop_project_active_views(request):
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    return HttpResponse(stop_project_active(body.get('project_id')))
+
+
+def add_project_active_views(request):
+    body_unicode = request.body.decode('utf-8')
+    body = json.loads(body_unicode)
+    return HttpResponse(add_project_active(body.get('project_id'), request.user))
