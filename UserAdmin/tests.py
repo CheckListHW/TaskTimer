41f8581f04ed154b0acfc620f6a2634a46a76057
Иван Косачев <kosach_1@live.com>
2021-03-08 14:01:22 +0700

    refactor

--- UserAdmin/tests.py
+++ UserAdmin/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, client
 
-# Create your tests here.
+import threading
+
+
+def login(index):
+
+    c = client.Client()
+    response = c.post('/', {
+        'username': 'commonuser' + str(index),
+        'password': '13579asd'
+    })
+    c.post('/project_active/add', {
+        'project_id': 1,
+    })
+    print(response)
+
+
+for i in range(1, 2):
+    t = threading.Thread(target=login, args=(i,))
+    t.start()
+
