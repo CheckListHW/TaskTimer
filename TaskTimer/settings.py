0176e847c156a9dd1ddc48f24ce1820af6e196c7
Иван Косачев <kosach_1@live.com>
2021-03-30 21:19:43 +0700

    static django

--- TaskTimer/settings.py
+++ TaskTimer/settings.py
@@ -78,16 +78,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
@@ -107,6 +98,7 @@ STATIC_URL = '/TaskTimer/frontend/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "/TaskTimer/frontend/static"),
     os.path.join(BASE_DIR, "frontend/static"),
+    os.path.join(BASE_DIR, "/djangoenv/lib/python3.7/site-packages/django/contrib/admin/static/"),
 ]
 
 STATIC_ROOT = '/home/woxel/TaskTimer/frontend/static'
