75ce7b03dc962c394320322d7a8d9040036aba14
Petr Obukhov <petr5542@gmail.com>
2021-04-26 19:58:11 +0700

    add Recovery

--- frontend/static/Welcome/js/login_js.js
+++ frontend/static/Welcome/js/login_js.js
@@ -12,13 +12,39 @@ new Vue ({
             confirn_passford: "",
         },
 
+        recoveryName: {
+            name: "",
+        },
+        getRecovery: {
+            cod: "",
+            password: "",
+            passwordRepeat: "",
+        },
+
         login_visible: true,
         register_visible: false,
+
+        isRecovery: false,
     },
     methods: {
         change: function() {
             this.login_visible = !this.login_visible;
             this.register_visible = !this.register_visible;
         },
+
+        recovery: function() {
+            this.isRecovery = true;
+        },
+
+        back: function() {
+            this.isRecovery = false;
+
+        },
+
+        ok: function() {
+            this.isRecovery = false;
+            this.login_visible = !this.login_visible;
+            this.register_visible = !this.register_visible;
+        }
     }
 })
